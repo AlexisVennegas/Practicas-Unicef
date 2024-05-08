c577870e5352a3eb96782e8e349b8a75ebf8cbfb
AlexisVennegas <erick.alexis.venegas@gmail.com>
2024-05-08 11:59:42 +0200

    feat: Randomize box order and assign colors to each box
    ```

--- js/boxs_random.js
+++ js/boxs_random.js
@@ -1,5 +1,12 @@
 const container_boxs = document.querySelector('.container_box');
 const boxs = Array.from(container_boxs.children);
 
+// aqui se ordenan los boxs de forma aleatoria
 container_boxs.innerHTML = '';
 boxs.sort(() => Math.random() - 0.5).forEach(box => container_boxs.appendChild(box));
+
+// aqui se les asigna un color a cada box
+boxs.forEach((box, index) => {
+    box.style.backgroundColor = `hsl(${index * 30}, 100%, 50%)`;
+}
+);
