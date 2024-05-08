9d89386c4e6fc974e5873bb7dd20af74786e6995
AlexisVennegas <erick.alexis.venegas@gmail.com>
2024-05-08 10:33:55 +0200

    Remove unused CSS code in index.html, add padding to container, and delete unnecessary files

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ const pinguino = "🐧​"
 for (let i = 0; i < texto.length; i++) {
     setTimeout(() => {
         parrafo.innerHTML += texto[i]
+        
+        if (texto.length == i + 1) {
+            setTimeout(() => {
+                window.location.href = "./boxs_random.html"
+            }, 3000)
+        }
     }, 60 * i)
-  
 }
